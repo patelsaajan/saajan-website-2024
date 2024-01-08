e32591a47c62f14b0b9a71dd8a8574ea40219833
Saajan Patel <saajanpatel@hotmail.com>
2024-01-08 20:20:44 +0000

    project section code test 1

--- src/app/projects/[projectName]/page.tsx
+++ src/app/projects/[projectName]/page.tsx
@@ -10,25 +10,26 @@ interface pagePromps {
 }
 
 export const generateStaticParams = async () => {
-  const posts = getPostMetadata(
-    path.join(process.cwd(), "/markdownFiles/projects/")
-  );
-  return posts.map((post) => ({ slug: post.slug }));
+  // const posts = getPostMetadata(
+  //   path.join(process.cwd(), "/markdownFiles/projects/")
+  // );
+  // return posts.map((post) => ({ slug: post.slug }));
 };
 
 const page = ({ params }: pagePromps) => {
   const slug = params.projectName;
   const post = getMarkdownContent(slug, "markdownFiles/projects/");
   return (
-    <Container maxWidth="lg">
-      <Typography
-        variant="h5"
-        sx={{ textAlign: "center", my: "40px", fontWeight: "600" }}
-      >
-        {post.data.title}
-      </Typography>
-      <Markdown>{post.content}</Markdown>
-    </Container>
+    // <Container maxWidth="lg">
+    //   <Typography
+    //     variant="h5"
+    //     sx={{ textAlign: "center", my: "40px", fontWeight: "600" }}
+    //   >
+    //     {post.data.title}
+    //   </Typography>
+    //   <Markdown>{post.content}</Markdown>
+    // </Container>
+    <>Hello</>
   );
 };
 
